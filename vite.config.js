b1e72c4a8e8397b1c15c7bcce3c737ef04407bab
Santiago <fsgreco@fastmail.com>
2023-01-22 12:56:15 +0100

    Fix CORS potential error

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: { port: 3000 }, // keep in sync with the FRONTEND_URL env var of Laravel docker container
   plugins: [vue()],
   resolve: {
     alias: {
